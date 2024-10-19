3365671ffbb122968a487b69ee65157a6d0e8426
Nashit <nazmulnashitofficial@gmail.com>
2024-10-20 03:00:33 +0600

    Six ways to do conditional rendering

--- Module_38_React_Core_Concept/explore-react-core-concept/src/Todo.jsx
+++ Module_38_React_Core_Concept/explore-react-core-concept/src/Todo.jsx
@@ -16,10 +16,49 @@
 // }
 
 
-//Conditional rendering: 2
-export default function Todo({task, isdone}) {
-   if(isdone){
-    return <li>Finished: {task}</li>
+// //Conditional rendering: 2
+// export default function Todo({task, isdone}) {
+//    if(isdone){
+//     return <li>Finished: {task}</li>
+//    }
+//    return <li>Work On: {task}</li>
+// }
+
+//Conditional rendering 3: ternary operator
+// export default function Todo({task, isdone}) {
+//    return (
+//       <li> {isdone? 'Finished' : 'Work On'} : {task}</li>
+//    )
+// }
+
+
+//Conditional rendering 4: && operator
+// export default function Todo({task, isdone}) {
+//     return (
+//         <li>{task} {isdone && ':Done'}</li>
+//     )
+// }
+
+
+
+//Conditional rendering 5: || operator
+// export default function Todo({task, isdone}) {
+//     return (
+//         <li>{task} {isdone || ':Do it'}</li>
+//     )
+// }
+
+
+
+
+// //Conditional rendering: 6
+export default function Todo({ task, isdone }) {
+   let listItem;
+   if (isdone) {
+      listItem = <li>Finished: {task}</li>
    }
-   return <li>Work On: {task}</li>
+   else {
+      listItem = <li>Work On: {task}</li>
+   }
+   return listItem;
 }
