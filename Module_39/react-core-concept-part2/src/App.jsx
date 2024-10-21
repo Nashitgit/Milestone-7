1efce9a9af17224f42851f54ddffed8c79e39218
Nashit <nazmulnashitofficial@gmail.com>
2024-10-22 01:36:09 +0600

    Different way to handle event in react

--- Module_39/react-core-concept-part2/src/App.jsx
+++ Module_39/react-core-concept-part2/src/App.jsx
@@ -2,13 +2,32 @@
 import './App.css'
 
 function App() {
-  
+
+  function handleButton() {
+    alert('Button clicked')
+  }
+
+  const handleButton2 = () => {
+    alert('Button 2 clicked')
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5)
+  }
 
   return (
     <>
+
+      <h3>React Core Concept 2</h3>
+      <button onClick={handleButton}>Click Me</button>
+      <button onClick={handleButton2}> Click 2</button>
+      <button onClick={() => { alert('Button 3 clicked') }}>Click 3</button>
       
-      <h1>React Core Concept 2</h1>
-      
+      {/* it can not be used like this while passing value. cause it is called instantly*/}
+      {/* <button onClick={addToFive(5)}>Click 4</button> */}
+
+      <button onClick={() => {addToFive(3)}}>Click 4</button>
+
     </>
   )
 }
