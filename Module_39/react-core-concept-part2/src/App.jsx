1cb94ab2aa633aaff43405b2eefdda3d6fc91db4
Nashit <nazmulnashitofficial@gmail.com>
2024-10-23 03:03:21 +0600

    5. Concept of UseEffect

--- Module_39/react-core-concept-part2/src/App.jsx
+++ Module_39/react-core-concept-part2/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './Counter'
 import Team from './Team'
+import Users from './Users'
 
 function App() {
 
@@ -21,6 +22,8 @@ function App() {
     <>
 
       <h3>React Core Concept 2</h3>
+
+      <Users></Users>
 
       <Team></Team>
       <Counter></Counter>
